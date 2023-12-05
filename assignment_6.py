aa1966bee2f9857d55adaf037625e062cbcec7d6
Arie Twigt <twigt.arie@gmail.com>
2023-12-05 11:58:34 +0100

    added examples and assignments for loops

--- assignment_6.py
+++ assignment_6.py
@@ -18,3 +18,47 @@ for name in names_list:
 # %%
 names_list_new
 # %%
+
+
+### ASSIGNMENT 2
+
+#%%
+import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL, 'nl_NL') 
+
+#%%
+today_date = datetime.date.today()
+
+
+# %%
+num_days = 10
+
+for x in range(1, num_days + 1):
+    new_date = today_date + datetime.timedelta(days=x)
+    
+    # get the name of the day name in the date
+    day_name = new_date.strftime("%A, %B")
+    print(day_name)
+
+
+
+# %% b.
+
+today_date = datetime.date.today()
+
+num_days = 1
+max_days = 10
+
+while num_days <= 10:
+    new_date = today_date + datetime.timedelta(days=num_days)
+
+    # get the name of the day name in the date
+    day_name = new_date.strftime("%A, %B")
+    print(day_name)
+
+    # 
+    num_days += 1
+
+# %%
